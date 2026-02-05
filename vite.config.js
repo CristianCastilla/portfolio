0d4b5328467b5a517200377a562c9c420ebd944e
Cristian Castilla <tu-email@ejemplo.com>
2026-02-05 12:32:48 +0100

    Configurando despliegue automático con Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
 
+import { resolve } from 'path'
+
 export default defineConfig({
   base: '/portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+      },
+    },
+  },
 })
